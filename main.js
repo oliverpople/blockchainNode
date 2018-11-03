da5b97bb45ac1f5b2ad0052c59b82c92e59fdb0b
oliverpople <oliverpople@gmail.com>
2018-11-03 16:12:10 -0700

    Added function to create and return sha256 hash out of block and previous block data. Store hash in block class contructor.

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ class Block {
     this.timestamp = Math.floor(Date.now() / 1000);
     this.data = data;
     this.prevHash = prevHash;
+    this.hash = this.getHash();
+  }
+
+  getHash() {
+    return sha(
+      JSON.stringify(this.data) + this.prevHash + this.index + this.timestamp
+    );
   }
 }
