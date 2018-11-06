4737b8e8d84ad15987a4e8c7ae1ac8b5d0562a7a
oliverpople <oliverpople@gmail.com>
2018-11-06 11:15:25 -0800

    Made Blockchain component responsible for calling /api/blockchain.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,27 +3,10 @@ import React, { Component } from "react";
 import BlockChain from "./Blockchain";
 
 class App extends Component {
-  state = {
-    response: ""
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  callApi = async () => {
-    const response = await fetch("/api/blockchain");
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-
   render() {
     return (
       <div className="App">
         <BlockChain />
-        <p>{this.state.response}</p>
       </div>
     );
   }
--- client/src/components/Blockchain.js
+++ client/src/components/Blockchain.js
@@ -3,22 +3,28 @@ import React, { Component } from "react";
 const Block = () => <h3> Block </h3>;
 
 class Blockchain extends Component {
-  // // Fetch the list on first mount
-  // componentDidMount() {
-  //   this.getList();
-  // }
-  //
-  // // Retrieves the list of items from the Express app
-  // getList = async () => {
-  //   const response = await fetch("/api/blockchain");
-  //   const body = await response.json();
-  //   console.log(body);
-  // };
+  state = {
+    response: ""
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/blockchain");
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
 
   render() {
     return (
       <div>
         <h2> BlockChain </h2>
+        <p>{this.state.response}</p>
         <Block />
       </div>
     );
