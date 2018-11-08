15b8977c6924e0b9fad4af7114fe30ef18620756
oliverpople <oliverpople@gmail.com>
2018-11-08 12:32:17 -0800

    Form now updates BlockChain on form submit.

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,23 +3,35 @@ import React, { Component } from "react";
 class Form extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inputValue: ""
+    };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+  updateInput(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.getNewBlockInputData(event.target.value);
+    this.props.getNewBlockInputData(this.state.inputValue);
   }
 
   render() {
     return (
-      <form onChange={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
+      <form>
+        <input
+          value={this.state.inputValue}
+          onChange={this.updateInput}
+          type="text"
+          name="name"
+        />
+        <input type="submit" onClick={this.handleSubmit} />
       </form>
     );
   }
